db2c8d9586cf09f07f52c7d01b859ae88e4ff766
ange-xlei <n11592524@qut.edu.au>
2026-05-01 14:09:10 +1000

    Fixing some of the broken files as well

---
+++ TaskTopia/src/test/java/com/tasktopia/SampleTest.java
@@ -0,0 +1,12 @@
+package com.tasktopia;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SampleTest {
+
+    @Test
+    void sampleTest() {
+        assertEquals(4, 2 + 2);
+    }
+}
