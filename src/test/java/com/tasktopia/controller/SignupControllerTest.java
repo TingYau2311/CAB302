82217af742d9bed8bf4ce2f6a083248cf4e3c75d
TingYau2311 <n12040568@qut.edu.au>
2026-05-19 14:34:52 +1000

    test login test

--- src/test/java/com/tasktopia/controller/SignupControllerTest.java
+++ src/test/java/com/tasktopia/controller/SignupControllerTest.java
@@ -1,4 +1,4 @@
-/*package com.tasktopia.controller;
+package com.tasktopia.controller;
 
 import com.tasktopia.model.Contact;
 import com.tasktopia.model.MockContactDAO;
@@ -172,4 +172,3 @@ class SignupControllerTest {
     }
 }
 
- */
